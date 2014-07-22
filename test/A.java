1e55fdff8001ae5cb397d8fc1d0b8f5e82109aeb
yutuer <yutuerzf@163.com>
2014-07-22 21:04:39 +0800

    a test

---
+++ test/A.java
@@ -0,0 +1,24 @@
+package arrayTest;
+
+
+public class A {
+	public static void main(String[] args) {
+//		Object lock = new Object();
+//		final Thread now = Thread.currentThread();
+//
+//		Thread t = new Thread() {
+			
+//			@Override
+//			public void run() {
+//				try {
+//					Thread.sleep(2000);
+//					LockSupport.unpark(now);
+//				} catch (InterruptedException e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		};
+//		t.start();
+//		LockSupport.park(lock);
+	}
+}
